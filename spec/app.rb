4067f4f76ab9893719272a699606f80f2baaa92d
SallyMulupi <mulupisally2gmail.com>
2022-08-14 21:12:26 +0300

    done

--- spec/app.rb
+++ spec/app.rb
@@ -1,5 +1,5 @@
-print "Hello world!"
-print "Hello world!"
-print "Hello world!"
+puts "Hello world!"
 
-pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+print "pass this test,please."
+p [1, 2, 3]
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
